ff866d7f562562bfebb586869128a8caed1597db
jefferdaniel2001 <jefferdaniel2001@gmail.com>
2024-06-17 04:08:27 +0000

    first commit

---
+++ Introdution/variables.py
@@ -0,0 +1,8 @@
+nombre = "Dilan Urrua"
+edad = 20;
+flot = 20.1;
+print(type(nombre));
+print(edad);
+print(flot);
+# Suggested code may be subject to a license. Learn more: ~LicenseLog:4155930173.
+print(type(flot));
